d7184bde2b683a24b98fc632b1c8aa8004d97a35
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2025-08-07 20:40:55 -0300

    .

---
+++ modulo1/clase3/6-compuertasLogicas.py
@@ -0,0 +1,27 @@
+# Las compuertas logicas, son una serie de elementos, que nos van a ayudar a "relacionar" varias condiciones
+
+# Operador AND
+# Devuelve True SOLAMENTE si las dos condiciones son verdaderas
+
+""" 
+True and True = True
+True and False = False
+False and True = False
+False and False = False
+"""
+
+# Operador OR
+# Devuelve True si cualquiera de las dos operaciones son True
+
+""" 
+True or True = True
+True or False = True
+False or True = True
+False or False = False
+"""
+
+# Operador NOT
+# Invierte el estado de veracidad de condiciones o booleanos.
+
+""" not True = False
+not False = True """
